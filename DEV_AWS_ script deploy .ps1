258fce842299fb303407169193c380c32209dc22
Eduard Ocheretnyi <82085302+o4edik@users.noreply.github.com>
2021-05-04 16:48:31 +0300

    Add files via upload

--- DEV_AWS_ script deploy .ps1
+++ DEV_AWS_ script deploy .ps1
@@ -2,15 +2,11 @@
 $credential =Get-Credential
 Enter-PSSession -ComputerName 3.135.137.226 -Credential $credential
 # Create web server
- Get-WindowsFeature -Name web*
  Install-WindowsFeature -Name web-server -IncludeManagementTools -IncludeAllSubFeature
  Import-Module -Name WebAdministration
-#Get-PSProvider
  # Create website
  cd c:\
-#ls .\inetpub
  cd .\inetpub
-# C:\inetpub> 
  New-Item -ItemType directory -Path testtask
  Write-Output '<head>HTML Example</head><br><body>testtask (Dev_AWS)</body>' |out-file .\testtask\index.html
  New-Website -Name testtask -PhysicalPath C:\inetpub\testtask -Port 80
