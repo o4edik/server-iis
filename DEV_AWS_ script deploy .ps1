f47d413e969946cc02a095c1715a68c2bfe0645e
Eduard Ocheretnyi <82085302+o4edik@users.noreply.github.com>
2021-05-04 16:42:22 +0300

    Add files via upload
    Creation IIS Server and site with PS

---
+++ DEV_AWS_ script deploy .ps1
@@ -0,0 +1,21 @@
+﻿# Enter to REmote Server
+$credential =Get-Credential
+Enter-PSSession -ComputerName 3.135.137.226 -Credential $credential
+# Create web server
+ Get-WindowsFeature -Name web*
+ Install-WindowsFeature -Name web-server -IncludeManagementTools -IncludeAllSubFeature
+ Import-Module -Name WebAdministration
+#Get-PSProvider
+ # Create website
+ cd c:\
+#ls .\inetpub
+ cd .\inetpub
+# C:\inetpub> 
+ New-Item -ItemType directory -Path testtask
+ Write-Output '<head>HTML Example</head><br><body>testtask (Dev_AWS)</body>' |out-file .\testtask\index.html
+ New-Website -Name testtask -PhysicalPath C:\inetpub\testtask -Port 80
+ New-WebAppPool -Name testtask
+ Start-IISSite -Name testtask
+ Install-WindowsFeature rsat-dns-server
+ Exit-PSSession
+
---
+++ PROD_AWS_ script deploy.ps1
@@ -0,0 +1,16 @@
+﻿# Enter to REmote Server
+$credential =Get-Credential
+Enter-PSSession -ComputerName 3.128.112.87 -Credential $credential
+# Create web server
+ Install-WindowsFeature -Name web-server -IncludeManagementTools -IncludeAllSubFeature
+ Import-Module -Name WebAdministration
+ # Create website
+ cd c:\
+ cd .\inetpub
+ New-Item -ItemType directory -Path testtask
+ Write-Output '<head>HTML Example</head><br><body>testtask (Prod_AWS)</body>' |out-file .\testtask\index.html
+ New-Website -Name testtask -PhysicalPath C:\inetpub\testtask -Port 80
+ New-WebAppPool -Name testtask
+ Start-IISSite -Name testtask
+ Install-WindowsFeature rsat-dns-server
+ Exit-PSSession
